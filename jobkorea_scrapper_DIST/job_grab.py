baa0dc31c6feb1ac1f05e891e931cec80c572806
Yosam Lee <yslee.gm@gmail.com>
2023-11-28 16:52:23 +0900

    password도 입력받도록 수정

--- jobkorea_scrapper_DIST/job_grab.py
+++ jobkorea_scrapper_DIST/job_grab.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 @app.route('/') # 접속하는 url
 def index():
     if request.method == "POST":
-        print(request.form.get('keyword')) # 안전하게 가져오려면 get
         keyword = request.form.get('keyword')
-        print(request.form.get('keyword'))
-        extract_job('nto0102', keyword)
-        return render_template('index.html', keyword=keyword)
+        password = request.form.get('password')
+        print(f"{keyword}, {password}")
+        result = extract_job('nto0102' if password==None else password, keyword)
+        return result
     elif request.method == "GET":
         keyword = request.args.get('keyword')
-        print(f"keyword = {request.args.get('keyword')}")
-        result = extract_job('nto0102', keyword)
+        password = request.args.get('password')
+        print(f"{keyword}, {password}")
+        result = extract_job('nto0102' if password==None else password, keyword)
         return result 
-    #return render_template('index.html', keyword=keyword)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port="5001", debug=True)
